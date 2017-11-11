bd11301f291b0e7a6a1e142748c902d265db3760
AlexYi7852 <3092342855@qq.com>
2017-11-11 14:34:37 +0800

    goods-component Done

--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -63,15 +63,13 @@
         if (!event._constructed) {
           return
         }
-        this.selectType = type
-        this.$emit('ratingtype.select', type)
+        this.$emit('select', type)
       },
       toggleContent (event) {
         if (!event._constructed) {
           return
         }
-        this.onlyContent = !this.onlyContent
-        this.$emit('content.toggle', this.onlyContent)
+        this.$emit('toggle', this.onlyContent)
       }
     }
   }
